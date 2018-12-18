0403d86e907378fa118f39c9c4efb35de5a180cf
Kay Wood <11405108+kwood15@users.noreply.github.com>
2018-12-18 15:20:17 +0000

    Add prop-types to Posts component

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,7 +1,6 @@
 import { FETCH_POSTS, NEW_POST } from'./types';
 
 export const fetchPosts = () => dispatch => {
-  console.log('fetching');
   fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
     .then(posts => dispatch({
